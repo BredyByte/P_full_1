44015ae53e64e38335ead0b2ab0e09640762c4a0
BredyByte <correodemimobil@gmail.com>
2024-11-20 18:26:03 +0100

    feat: some styles changes

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -20,16 +20,17 @@ function setupSearchForm() {
 
                 if (input) {
                     try {
-                        await fetchImages('Horses');
+                        await fetchImages(input);
                         searchInput.value = '';
                     } catch (error) {
-                        console.error('Failed to send message:', error);
+                        console.error('Failed to fetch images:', error);
                     }
                 }
             }
         };
     }
 }
+
 
 async function handleResponse(response, onSuccess) {
     if (!response.ok) {
@@ -76,8 +77,40 @@ async function fetchImages(query) {
         const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=10&client_id=${keys.accessKey}`;
         makeRequest(url, (data) => {
             console.log('Received data:', data);
+            if (data.results) {
+                renderGallery(data.results);
+            }
         });
     } catch (error) {
         console.error("Error fetching images:", error);
     }
 }
+
+function renderGallery(images) {
+    const gallery = document.getElementById('gallery');
+
+    if (gallery) {
+        gallery.innerHTML = '';
+        const markup = createGalleryMarkup(images);
+        gallery.insertAdjacentHTML('beforeend', markup);
+    }
+}
+
+function createGalleryMarkup(images) {
+    return images
+        .map((image) => {
+            const highQualityImageUrl = `${image.urls.raw}&q=80&w=1280&fit=max`; // Уменьшили ширину до 1280
+            return `
+            <a href="${image.links.html}?utm_source=your_app_name&utm_medium=referral" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="${highQualityImageUrl}" alt="Photo by ${image.user.name}" class="gallery__image">
+                    <figcaption class="gallery__caption">Photo by ${image.user.name}</figcaption>
+                </figure>
+            </a>
+            `;
+        })
+        .join('');
+}
+
+
+
