710d99a5d0beadeba922f7f42b453be5cd4eca30
BredyByte <correodemimobil@gmail.com>
2024-11-21 19:20:14 +0100

    feat: favorite btn check if exist in ls

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -64,16 +64,14 @@ function setupFavoriteEvent() {
 
                 if (!isAlreadyFavorite) {
                     favorites.push({ id: imageId, url: imageUrl, author: imageAuthor });
-
                     localStorage.setItem('favorites', JSON.stringify(favorites));
 
                     console.log(`Image ${imageId} added to favorites.`);
+                    updateFavoriteButtonState(favoriteButton, true);
                 } else {
                     console.log(`Image ${imageId} is already in favorites.`);
                 }
             }
-
-            return;
         }
     });
 }
@@ -93,7 +91,6 @@ function setupFavoritesGalleryButton() {
 
     if (favoritesButton) {
         favoritesButton.addEventListener("click", () => {
-            // Переход на страницу с избранными фотографиями
             window.location.href = '/favorites.html';
         });
     }
@@ -193,16 +190,21 @@ async function appendToGallery(images) {
 
 async function createGalleryMarkup(images) {
     const isLoggedIn = await isUserLoggedIn();
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
     return images
         .map((image) => {
             const highQualityImageUrl = `${image.urls.raw}&q=80&w=1280&fit=max`;
+            const isFavorite = favorites.some(fav => fav.id === image.id);
+            const starButtonClass = isFavorite ? 'btn-danger' : 'btn-secondary';
+            const disabledAttribute = isFavorite ? 'disabled' : '';
             const starButton = isLoggedIn
-                ? `<button class="btn btn-secondary gallery__favorite"
+                ? `<button class="btn ${starButtonClass} gallery__favorite"
                            role="button"
                            data-image-id="${image.id}"
                            data-image-url="${highQualityImageUrl}"
-                           data-image-author="${image.user.name}">
+                           data-image-author="${image.user.name}"
+                           ${disabledAttribute}>
                         <i class="fa-regular fa-star"></i>
                    </button>`
                 : '';
@@ -240,4 +242,15 @@ function setupInfiniteScroll() {
     });
 }
 
+
+
+function updateFavoriteButtonState(button, isFavorite) {
+    if (isFavorite) {
+        button.classList.add('btn-danger');
+        button.disabled = true;
+    } else {
+        button.classList.remove('btn-danger');
+        button.disabled = false;
+    }
+}
 
